ff86ce4492bcb6b5d6c5dbcbf7c5362e11b78578
Anazodo Vanessa <anazodovanessa19@gmail.com>
2024-03-11 17:23:09 +0100

    pricing

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const slider = document.getElementById("myRange");
-const output = document.getElementById("price");
+const output = document.getElementById("monthly-price");
+const toggleBtn = document.getElementById("switch-toggle");
+const yearlyPrice = document.getElementById("yearly-price");
 
 output.innerHTML = slider.value;
+console.log(slider.value);
 
-slider.oninput = function () {
-  output.innerHTML = this.value;
-};
+slider.addEventListener("input", () => {
+  output.innerHTML = slider.value;
+});
+slider.addEventListener("input", () => {
+  yearlyPrice.innerHTML = slider.value * 12 * 0.25;
+});
+
+toggleBtn.addEventListener("click", () => {
+  if (yearlyPrice.style.display === "none") {
+    yearlyPrice.style.display = "inline";
+    output.style.display = "none";
+  } else {
+    yearlyPrice.style.display = "none";
+    output.style.display = "inline";
+  }
+});
